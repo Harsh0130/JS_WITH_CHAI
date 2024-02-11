d56ff59833c2df6ab72f2079cd69428a02c029fc
HARSH KUMAR TIWARY <38390441+Harsh0130@users.noreply.github.com>
2024-02-11 12:42:25 +0000

    coversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+
+//"33" => 33
+// "44abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=>ture 0=> false
+// "" => false
+//"harsh"=>true
+
+let num = 33
+
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log(typeof(stringNumber));

c7d614201b16ab176326c7aafdbc42ba5e047d0f
HARSH KUMAR TIWARY <38390441+Harsh0130@users.noreply.github.com>
2024-02-12 08:17:48 +0000

    conversion Operarion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 //"33" => 33
@@ -16,7 +16,7 @@ console.log(typeof valueInNumber);
 let isLoggedIn =1
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1=>ture 0=> false
@@ -26,5 +26,47 @@ console.log(booleanIsLoggedIn);
 let num = 33
 
 let stringNumber = String(num)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// ****Operation****
+
+let value =3
+let neg = -value
+// console.log(neg);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 ="Hello"
+let str2 = " Harsh"
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1 , num2,num3
+
+num1 = num2 = num3 =2+2
+// console.log(num1);
+
+let counter = 100
+++counter
+console.log(counter);
